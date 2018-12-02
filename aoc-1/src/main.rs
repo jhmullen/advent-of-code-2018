1ed99913b551ef68f09c38208f9abc7bc399af89
jhmullen <jimmy.mullen@gmail.com>
2018-12-02 14:44:05 -0500

    solves part 2 of day 1

--- aoc-1/src/main.rs
+++ aoc-1/src/main.rs
@@ -1,8 +1,14 @@
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
+use std::collections::HashSet;
 
 fn main() {
+  part1();
+  part2();
+}
+
+fn part1() {
   let f = File::open("input.txt").expect("io error");
   let reader = BufReader::new(f);
 
@@ -14,6 +20,34 @@ fn main() {
     freq += diff;
   }
 
-  println!("{}", freq);
+  println!("part 1: {}", freq);
 
-}
+} 
+
+fn part2() {
+  let mut f = File::open("input.txt").expect("io error");
+
+  let mut freq = 0;
+  let mut contents = String::new();
+  let mut done = false;
+  let mut totals = HashSet::new();
+
+  f.read_to_string(&mut contents).expect("reading error");
+
+  totals.insert(0);
+
+  while !done {
+    for line in contents.lines() {
+      let diff:i32 = line.parse::<i32>().unwrap();
+      freq += diff;
+      if totals.contains(&freq) {
+        done = true;
+        println!("part 2: {}", freq);
+        break;
+      }
+      else {
+        totals.insert(freq);
+      }
+    }
+  }
+} 
